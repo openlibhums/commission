5561e4d9e0985e2bab7f5eda4b55909f71e38d3a
Andy Byers <a.byers@bbk.ac.uk>
2019-09-11 12:44:14 +0100

    Added new message sent field.

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ class CommissionedArticle(models.Model):
     article = models.ForeignKey('submission.Article')
     commissioning_editor = models.ForeignKey('core.Account')
     commissioned = models.DateTimeField(default=timezone.now)
+    message_sent = models.DateTimeField(null=True, blank=True)
 
     def status(self):
         return self.article.stage
