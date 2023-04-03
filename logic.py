efca68720f98a8f8984d3e625060338891ca72f8
Andy Byers <a.byers@bbk.ac.uk>
2023-04-03 16:39:13 +0100

    WIP updates to the commission plugin

--- logic.py
+++ logic.py
@@ -1,10 +1,39 @@
 from utils import setting_handler, render_template
-from plugins.commission import plugin_settings
+from core import models
+from plugins.commission import models as commission_models
+
+from django.contrib import messages
+from django.shortcuts import reverse
+
+def remove_author_from_article(request, article, author_id):
+    try:
+        author = models.Account.objects.get(pk=author_id)
+    except models.Account.DoesNotExist:
+        messages.add_message(
+            request,
+            messages.WARNING,
+            'Author with this ID was not found.'
+        )
+        return
+
+    if author not in article.authors.all():
+        messages.add_message(
+            request,
+            messages.WARNING,
+            'This author is not in the list of authors for this article.'
+        )
+    else:
+        article.authors.remove(author)
+        messages.add_message(
+            request,
+            messages.SUCCESS,
+            'Author removed from article.'
+        )
 
 
 def render_commission_email(request, commissioned_article):
-    setting = setting_handler.get_plugin_setting(
-        plugin_settings.get_self(),
+    setting = setting_handler.get_setting(
+        'plugin:commission',
         'commission_article',
         request.journal,
     )
@@ -22,3 +51,51 @@ def render_commission_email(request, commissioned_article):
     )
 
     return template_html
+
+
+def get_rendered_templates(request, comm_article):
+    rendered_templates = []
+    decision_url = reverse(
+        'commissioned_author_decision',
+        kwargs={'commissioned_article_id': comm_article.pk}
+    )
+    context = {
+        'url': request.journal.site_url(path=decision_url),
+        'commissioned_article': comm_article,
+    }
+    if comm_article.article:
+        section_templates = commission_models.CommissionTemplate.objects.filter(
+            section=comm_article.article.section,
+        )
+        for template in section_templates:
+            rendered_templates.append(
+                {
+                    'template_identifier': template.pk,
+                    'template_name': template.name,
+                    'content': render_template.get_message_content(
+                        request,
+                        context,
+                        template.template,
+                        template_is_setting=True,
+                    ),
+                }
+            )
+
+    default_template = setting_handler.get_setting(
+        'plugin:commission',
+        'commission_article',
+        request.journal,
+    )
+    rendered_templates.append(
+        {
+            'template_identifier': "default",
+            'template_name': "Janeway Default Template",
+            'content': render_template.get_message_content(
+                request,
+                context,
+                default_template.processed_value,
+                template_is_setting=True,
+            ),
+        }
+    )
+    return rendered_templates
--- views.py
+++ views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib import messages
 from django.utils import timezone
 
-from plugins.commission import forms, models, utils, logic as commission_logic
-from security.decorators import editor_user_required
+from plugins.commission import forms, models, logic as commission_logic
+from security.decorators import editor_user_required, any_editor_user_required
 from submission.forms import AuthorForm
 from submission import logic
-from utils import shared
+from utils import shared, render_template
 from security.decorators import has_journal
-from utils import notify_helpers
+from utils import notify_helpers, setting_handler
+from core.forms import QuickUserForm
+from core import models as core_models
 
 
 @has_journal
@@ -19,9 +21,32 @@ def index(request):
     :param request: HttpRequest object
     :return: HttpResponse or HttpRedirect
     """
-    commissioned_articles = models.CommissionedArticle.objects.all()
+    commissioned_articles = models.CommissionedArticle.objects.all().exclude(
+        author_decision_editor_check=True,
+        author_decision='declined',
+    )
     template = 'commission/index.html'
     context = {
+        'commissioned_articles': commissioned_articles,
+    }
+    return render(request, template, context)
+
+
+@has_journal
+@editor_user_required
+def declined_commissions(request):
+    """
+    Displays a list of reports for a user to select.
+    :param request: HttpRequest object
+    :return: HttpResponse or HttpRedirect
+    """
+    commissioned_articles = models.CommissionedArticle.objects.filter(
+        author_decision_editor_check=True,
+        author_decision='declined',
+    )
+    template = 'commission/index.html'
+    context = {
+        'declined_articles': True,
         'commissioned_articles': commissioned_articles,
     }
     return render(request, template, context)
@@ -106,7 +131,7 @@ def commissioned_article(request, commissioned_article_id):
 
         if 'delete_author' in request.POST:
             delete_author_id = request.POST.get('delete_author')
-            utils.remove_author_from_article(
+            commission_logic.remove_author_from_article(
                 request,
                 commissioned_article.article,
                 delete_author_id
@@ -182,3 +207,190 @@ def commissioned_article(request, commissioned_article_id):
     }
 
     return render(request, template, context)
+
+
+@any_editor_user_required
+def commission_article_owner(request):
+    """
+    Allows an editor to set up a commissioned article's owner.
+    """
+    new_author_form = QuickUserForm()
+
+    if request.POST:
+        owner = None
+        if 'save_new_author' in request.POST:
+            new_author_form = QuickUserForm(
+                request.POST,
+            )
+            if new_author_form.is_valid():
+                owner = new_author_form.save()
+                owner.is_active = True
+                owner.save()
+                owner.add_account_role('author', request.journal)
+
+        elif 'select_author' in request.POST:
+            owner_id = request.POST.get('select_author')
+            owner = get_object_or_404(
+                core_models.Account,
+                pk=owner_id,
+            )
+            owner.add_account_role('author', request.journal)
+
+        if owner:
+            comm_article = models.CommissionedArticle.objects.create(
+                commissioning_editor=request.user,
+                commissioned_author=owner,
+                journal=request.journal,
+            )
+            return redirect(
+                reverse(
+                    'commissioned_article_details',
+                    kwargs={
+                        'commissioned_article_id': comm_article.pk,
+                    }
+                )
+            )
+
+    template = 'commission/commission_article_owner.html'
+    context = {
+        'new_author_form': new_author_form,
+    }
+    return render(
+        request,
+        template,
+        context,
+    )
+
+
+@any_editor_user_required
+def commissioned_article_details(request, commissioned_article_id):
+    comm_article = get_object_or_404(
+        models.CommissionedArticle,
+        pk=commissioned_article_id,
+    )
+
+    duplicate_commissions = models.CommissionedArticle.objects.filter(
+        commissioned_author=comm_article.commissioned_author,
+    ).exclude(
+        pk=comm_article.pk,
+    )
+
+    article_form = forms.CommissionArticle(
+        instance=comm_article.article,
+        journal=request.journal,
+    )
+    deadline_form = forms.DeadlineForm(
+        instance=comm_article,
+    )
+
+    if request.POST:
+        if 'html' in request.POST:
+            email_content = request.POST.get('html')
+            template_identifier = request.POST.get('template_id', 'default')
+
+            if not template_identifier == 'default':
+                template = get_object_or_404(
+                    models.CommissionTemplate,
+                    pk=template_identifier,
+                )
+                subject = template.subject
+            else:
+                subject = f'{request.journal.name} Commissioned Article'
+
+            notify_helpers.send_email_with_body_from_user(
+                request,
+                subject,
+                comm_article.commissioned_author.email,
+                email_content,
+            )
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                'Message sent.',
+            )
+            comm_article.message_sent = timezone.now()
+            comm_article.save()
+            return redirect(
+                reverse(
+                    'commissioned_article_details',
+                    kwargs={
+                        'commissioned_article_id': comm_article.pk,
+                    }
+                )
+            )
+
+        if 'cancel_commission' in request.POST:
+            if comm_article.article:
+                comm_article.article.delete()
+            comm_article.delete()
+            return redirect(
+                reverse(
+                    'commission_index'
+                )
+            )
+        if 'archive_commission' in request.POST:
+            comm_article.author_decision_editor_check = True
+            comm_article.save()
+            messages.add_message(
+                request,
+                messages.INFO,
+                'Commission has been archived.'
+            )
+            return redirect(
+                reverse(
+                    'commission_index'
+                )
+            )
+        if 'save_section' in request.POST:
+            article_form = forms.CommissionArticle(
+                request.POST,
+                instance=comm_article.article,
+                journal=request.journal,
+            )
+            deadline_form = forms.DeadlineForm(
+                request.POST,
+                instance=comm_article,
+            )
+            if article_form.is_valid() and deadline_form.is_valid():
+                deadline_form.save()
+                article = article_form.save(commit=False)
+                article.owner = comm_article.commissioned_author
+                article.save()
+                comm_article.article = article
+                comm_article.save()
+
+                messages.add_message(
+                    request,
+                    messages.SUCCESS,
+                    'Details saved.',
+                )
+                return redirect(
+                    reverse(
+                        'commissioned_article_details',
+                        kwargs={
+                            'commissioned_article_id': comm_article.pk,
+                        }
+                    )
+                )
+
+    template = 'commission/commission_article_details.html'
+    context = {
+        'comm_article': comm_article,
+        'duplicate_commissions': duplicate_commissions,
+        'article_form': article_form,
+        'deadline_form': deadline_form,
+        'rendered_templates': commission_logic.get_rendered_templates(
+            request,
+            comm_article,
+        ),
+
+    }
+    return render(
+        request,
+        template,
+        context,
+    )
+
+
+def commissioned_author_decision(request, commissioned_article_id):
+    pass
